4c4d9bb5344ce897dc8d32b679d406ab78cef793
John-Gathirua99 <133219769+John-Gathirua99@users.noreply.github.com>
2025-03-07 20:17:06 -0930

    Update settings.py

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$&eym&9^c0c0tz&e3nthdsphb%=p54r77d4jj_55&hr+&v3ye7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
